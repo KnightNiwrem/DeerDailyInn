1bec992f0695db23442d4c67d0d996018aabfeb2
KnightNiwrem <merwinwee@gmail.com>
2018-05-23 14:58:05 +0800

    Update pool of item codes and add wtb action

---
+++ controllers/telegram/wtb.js
@@ -0,0 +1,150 @@
+const _ = require('lodash');
+const Promise = require('bluebird');
+const User = require('../../models/user');
+
+const makeUnregisteredMessage = (chatId) => {
+  const unregisteredText = `Hi, you don't seem to \
+be registered yet! Do /start to register first!`;
+  
+  const unregisteredMessage = JSON.stringify({
+    chat_id: chatId,
+    text: unregisteredText
+  });
+  return unregisteredMessage;
+};
+
+const makeBadArgumentMessage = (chatId) => {
+  const text = `Bad format. Should be /wtb_{item code}_{quantity}_{price}`;
+
+  const message = JSON.stringify({
+    chat_id: chatId,
+    text: text
+  });
+  return message;
+};
+
+const makeWantToBuyRequest = (chtwrsToken, itemCode, quantity, price) => {
+  const message = JSON.stringify({  
+    token: chtwrsToken,  
+    action: "wantToBuy",  
+    payload: {  
+      itemCode: itemCode, 
+      quantity: quantity,
+      price: price,
+      exactPrice: true
+    }  
+  });
+  return message;
+};
+
+const itemCodeToNameEntries = [
+  ['01', 'Thread'],
+  ['02', 'Stick'],
+  ['03', 'Pelt'],
+  ['04', 'Bone'],
+  ['05', 'Coal'],
+  ['06', 'Charcoal'],
+  ['07', 'Powder'],
+  ['08', 'Iron ore'],
+  ['09', 'Cloth'],
+  ['13', 'Magic stone'],
+  ['19', 'Steel'],
+  ['20', 'Leather'],
+  ['21', 'Bone powder'],
+  ['22', 'String'],
+  ['23', 'Coke'],
+  ['39', 'Stinky Sumac'],
+  ['40', 'Mercy Sassafras'],
+  ['41', 'Cliff Rue'],
+  ['42', 'Love Creeper'],
+  ['43', 'Wolf Root'],
+  ['44', 'Swamp Lavender'],
+  ['45', 'White Blossom'],
+  ['46', 'Ilaves'],
+  ['47', 'Ephijora'],
+  ['48', 'Storm Hyssop'],
+  ['49', 'Cave Garlic'],
+  ['50', 'Yellow Seed'],
+  ['51', 'Tecceagrass'],
+  ['52', 'Spring Bay Leaf'],
+  ['53', 'Ash Rosemary'],
+  ['54', 'Sanguine Parsley'],
+  ['55', 'Sun Tarragon'],
+  ['56', 'Maccunut'],
+  ['57', 'Dragon Seed'],
+  ['58', 'Queen\'s Pepper'],
+  ['59', 'Plasma of abyss'],
+  ['60', 'Ultramarine dust'],
+  ['61', 'Ethereal bone'],
+  ['62', 'Itacory'],
+  ['63', 'Assassin Vine'],
+  ['64', 'Kloliarway'],
+  ['501', 'Wrapping'],
+  ['506', 'Bottle of Remedy'],
+  ['508', 'Bottle of Poison'],
+  ['p01', 'Vial of Rage'],
+  ['p02', 'Potion of Rage'],
+  ['p03', 'Bottle of Rage'],
+  ['p04', 'Vial of Peace'],
+  ['p05', 'Potion of Peace'],
+  ['p06', 'Bottle of Peace'],
+  ['p07', 'Vial of Greed'],
+  ['p08', 'Potion of Greed'],
+  ['p09', 'Bottle of Greed'],
+  ['p10', 'Vial of Nature'],
+  ['p11', 'Potion of Nature'],
+  ['p12', 'Bottle of Nature'],
+  ['s01', '📕Scroll of Rage'],
+  ['s02', '📕Scroll of Peace'],
+  ['tch', 'Torch']
+];
+
+const itemCodes = Set(itemCodeToNameEntries.map(entry => entry[0]));
+const itemNames = itemCodeToNameEntries.map(entry => entry[1]);
+const searchTermToNameMap = new Map([
+  ...itemCodeToNameEntries,
+  ...itemNames.map(name => [normalizeItemName(name), name])
+]);
+
+const wtb = (params) => {
+  if (_.isNil(params.bot)) {
+    return Promise.reject('Rejected in wtb: Bot cannot be missing');
+  }
+  if (_.isNil(params.telegramId) || _.isNil(params.chatId)) {
+    return Promise.reject('Rejected in wtb: Missing telegram user id or chat id');
+  }
+
+  const bot = params.bot;
+  const chatId = params.chatId;
+  const telegramId = params.telegramId;
+
+  const options = params.options;
+  const [itemCode, quantityInput, priceInput] = options;
+  const quantity = Number(quantityInput);
+  const price = Number(priceInput);
+
+  const isValidItemCode = !_.isEmpty(itemCode) && itemCodes.has(itemCode);
+  const isValidQuantity = !_.isEmpty(quantity) && _.isFinite(quantity) && _.isSafeInteger(quantity) && (quantity > 0);
+  const isValidPrice = !_.isEmpty(price) && _.isFinite(price) && _.isSafeInteger(price) && (price > 0);
+  if (!isValidCode || !isValidQuantity || !isValidPrice) {
+    const message = makeBadArgumentMessage(chatId);
+    return bot.sendTelegramMessage('sendMessage', message);
+  }
+
+  return User.query()
+  .where({ telegramId })
+  .first()
+  .then((user) => {
+    const isSuccess = !_.isNil(user) && !_.isEmpty(user.chtwrsId);
+    if (_.isNil(user) || _.isEmpty(user.chtwrsToken)) {
+      const message = makeUnregisteredMessage(chatId);
+      bot.sendTelegramMessage('sendMessage', message);
+      return Promise.reject(`Rejected in wtb: User ${telegramId} is not registered.`);
+    }
+
+    const request = makeWantToBuyRequest(user.chtwrsToken, itemCode, quantity, price);
+    return bot.sendChtwrsMessage(request);
+  });
+};
+
+module.exports = wtb;
